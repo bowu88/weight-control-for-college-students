b918d6a37d9e3e74eb38012d38cee10ea4404142
Zeno Zeng <zenoofzeng@gmail.com>
2014-11-10 19:39:31 +0800

    update README

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-pandoc README.md --toc --standalone --html5  --include-in-header forkmeongithub.html  -c styles.css > index.html
+pandoc index.md --toc --standalone --html5  --include-in-header forkmeongithub.html  -c styles.css > index.html
